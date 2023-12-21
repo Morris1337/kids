2bd0349a7470a71d61a80428069e860498a5397f
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-12-21 17:59:55 +0200

    change map output to dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: path.resolve(__dirname, 'js/index.js'),
     output: {
-        path: path.resolve(__dirname, 'output'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
     mode: 'development',
